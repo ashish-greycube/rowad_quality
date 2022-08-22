bf87e26100acbbf450de78ecf89eb522a00092f6
vr-greycube <vijay@greycube.in>
2022-08-22 11:38:27 +0530

    feat: add btn link to expense entry
    add company filter in department link

--- rowad_quality/rowad_quality/doctype/amount_request/amount_request.js
+++ rowad_quality/rowad_quality/doctype/amount_request/amount_request.js
@@ -27,4 +27,28 @@ frappe.ui.form.on('Amount Request', {
 		frm.set_value("total_amount", total_amount)
 		frm.set_value("balance_after_request", total_amount + (frm.doc.party_balance || 0))
 	},
+
+	refresh: function (frm) {
+		if (frm.doc.docstatus == 1) {
+			frm.add_custom_button(__('Make Expense Entry'), function () {
+				var expense = frappe.model.make_new_doc_and_get_name('Expense Entry');
+				expense = locals['Expense Entry'][expense];
+				expense.expense_type = 'Employee Petty Cash';
+				expense.company = frm.doc.company;
+				expense.party_type = 'Employee';
+				expense.party = frm.doc.employee;
+				frappe.set_route('Form', 'Expense Entry', expense.name);
+			});
+		}
+	},
+	setup: function (frm) {
+		frm.set_query("department", function () {
+			return {
+				filters: {
+					company: frm.doc.company
+				}
+			}
+		});
+	}
+
 });
